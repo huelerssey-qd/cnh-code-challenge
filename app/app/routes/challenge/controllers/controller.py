e898405a95799e12284c722108f5a7c7cee22f8e
Daniel <ramondaniel48@gmail.com>
2025-07-31 15:46:30 -0300

    feat: atualiza desafio com implementação e testes

---
+++ app/app/routes/challenge/controllers/controller.py
@@ -0,0 +1,24 @@
+from app.routes.challenge.services.service import ChallengeService
+from app.logs.setup_logger import LOGGER
+class ChallengeController:
+    def __init__(self, service):
+        self.logger = LOGGER
+        self.service = ChallengeService()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def controller_challenge(self, request):
+        pass
